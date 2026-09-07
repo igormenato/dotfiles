59bb1c28255d3f73bafedf6b3c2e078af7b2b813
Igor Fortes Oliveira Lobo <igormenato@gmail.com>
2026-09-06 21:33:12 -0300

    Add Intel Quartus Prime Lite environment variables and paths

--- dot_config/private_fish/conf.d/envs.fish
+++ dot_config/private_fish/conf.d/envs.fish
@@ -1,1 +1,5 @@
 set -gx OPENCODE_ENABLE_EXA 1
+
+# Intel Quartus Prime Lite
+set -gx QSYS_ROOTDIR "$HOME/altera_lite/25.1std/quartus/sopc_builder/bin"
+set -gx QUARTUS_ROOTDIR "$HOME/altera_lite/25.1std/quartus"
--- dot_config/private_fish/conf.d/paths.fish
+++ dot_config/private_fish/conf.d/paths.fish
@@ -1,3 +1,6 @@
 fish_add_path $HOME/.opencode/bin
 
 fish_add_path $HOME/.local/bin
+
+# Intel Quartus Prime Lite
+fish_add_path $HOME/altera_lite/25.1std/quartus/bin $HOME/altera_lite/25.1std/qprogrammer/bin $HOME/altera_lite/25.1std/questa_fse/bin $HOME/altera_lite/25.1std/nios2eds/bin
